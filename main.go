87c18d7e158438f84fa531fdeb8668a5e922b5e1
Christophe Larsonneur <clarsonneur@gmail.com>
2017-09-26 23:10:59 +0200

    Get real data values from SAS.mdb

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"os"
-	"time"
+//	"os"
+//	"time"
 
 	"github.com/go-ole/go-ole"
 	"github.com/go-ole/go-ole/oleutil"
 	_ "github.com/mattn/go-adodb"
 )
+
+type Materiel struct {
+	RefProduit sql.NullString
+	TypeProduit sql.NullString
+	Marque sql.NullString
+	DescriptionProduit sql.NullString
+	NoSerie sql.NullString
+	Observations sql.NullString
+	Absent sql.NullBool
+	Rebuter sql.NullBool
+	Proprietaire sql.NullString
+	Constructeur sql.NullString
+	Capacite sql.NullFloat64
+	PS sql.NullInt64 //en bars
+	PE sql.NullInt64 //en bars
+	DatePremEpreuve sql.NullString
+	DateDernEpreuve sql.NullString
+
+}
+
+type Materiels map[string]Materiel
 
 func createMdb(f string) error {
 	unk, err := oleutil.CreateObject("ADOX.Catalog")
@@ -30,29 +51,29 @@ func createMdb(f string) error {
 func main() {
 	ole.CoInitialize(0)
 
-	f := "./example.mdb"
+	f := "./SAS.mdb"
 
-	os.Remove(f)
+//	os.Remove(f)
 
-	err := createMdb(f)
+/*	err := createMdb(f)
 	if err != nil {
 		fmt.Println("create mdb", err)
 		return
 	}
-
+*/
 	db, err := sql.Open("adodb", "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + f + ";")
 	if err != nil {
 		fmt.Println("open", err)
 		return
 	}
 
-	_, err = db.Exec("create table foo (id int not null primary key, name text not null, created datetime not null)")
+/*	_, err = db.Exec("create table foo (id int not null primary key, name text not null, created datetime not null)")
 	if err != nil {
 		fmt.Println("create table", err)
 		return
 	}
-
-	tx, err := db.Begin()
+*/
+/*	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -65,15 +86,16 @@ func main() {
 	defer stmt.Close()
 
 	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i), time.Now())
+		_, err = stmt.Exec(i, fmt.Sprintf("blabla %d", i), time.Now())
 		if err != nil {
 			fmt.Println("exec", err)
 			return
 		}
 	}
 	tx.Commit()
-
-	rows, err := db.Query("select id, name, created from foo")
+*/
+	matos := make(Materiels)
+	rows, err := db.Query("select * from Materiel")
 	if err != nil {
 		fmt.Println("select", err)
 		return
@@ -81,14 +103,16 @@ func main() {
 	defer rows.Close()
 
 	for rows.Next() {
-		var id int
-		var name string
-		var created time.Time
-		err = rows.Scan(&id, &name, &created)
+		var p Materiel
+		err = rows.Scan(&p.RefProduit, &p.TypeProduit, &p.Marque, &p.DescriptionProduit, &p.NoSerie, &p.Observations,
+			&p.Absent, &p.Rebuter, &p.Proprietaire, &p.Constructeur, &p.Capacite, &p.PS, &p.PE, &p.DatePremEpreuve,
+			&p.DateDernEpreuve)
 		if err != nil {
 			fmt.Println("scan", err)
 			return
 		}
-		fmt.Println(id, name, created)
+		matos[p.RefProduit.String] = p
+		fmt.Printf("%s - %s\n", p.RefProduit.String, p.TypeProduit.String)
 	}
+	fmt.Printf("\n%d enregistrements lus.\n", len(matos))
 }
